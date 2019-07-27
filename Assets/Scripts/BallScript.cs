6178afdf1b43d05f2ec2c8f82b829796fe61aa23
Tamir <1hotlev@gmail.com>
2019-07-27 17:32:03 +0300

    files added
    Materials, Scenes, Scripts, Terrain.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+    #region Variables
+    private bool IsBallOnGround = true;
+    private float DistanceToGround = 0.5f;
+    private Rigidbody rb;
+
+
+    public int num;
+    
+    #endregion
+
+    private void Start()
+    {
+        rb = FindObjectOfType<Rigidbody>();
+    }
+
+    void Update () {
+        if (IsBallOnGround)
+            PickUpBall();
+
+        CheckWhenBallGrounding();
+    }
+
+    private void PickUpBall()
+    {
+        rb.AddForce(0, num, 0);    
+    }
+
+    private void CheckWhenBallGrounding()
+    {
+        IsBallOnGround = Physics.Raycast(transform.position, Vector3.down, DistanceToGround);
+    }
+}
